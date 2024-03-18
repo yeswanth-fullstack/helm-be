4ccc7ff1335660b0e5ae88a5daa16c1bdda550da
Ubuntu <xloguser@xlog.w0z14t3rtyzelisftb0prdyyhe.parx.internal.cloudapp.net>
2024-03-18 10:58:51 +0000

    added a new api

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route('/api')
+def content():
+    data = [{"name": "Yeswanth", "message": "Hello"}, {"name": "Others", "message": "Hi"}]
+    return jsonify(data)
 
