2ab84b7ecbc743cb178390c3e5a4ff72b2992843
Ubuntu <xloguser@xlog.w0z14t3rtyzelisftb0prdyyhe.parx.internal.cloudapp.net>
2024-03-19 10:34:37 +0000

    adde cors related changes

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/')
+@cross_origin()
 def hello():
     return 'Hello, World!'
 
 @app.route('/api')
+@cross_origin()
 def content():
     data = [{"name": "Yeswanth", "message": "Hello"}, {"name": "Others", "message": "Hi"}]
     return jsonify(data)
